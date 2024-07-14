3e85592b42ab7d3425f53a0b8f7d67a88b586fe8
yanligao <yanli.gao@gmail.com>
2024-07-14 19:57:51 -0400

    update flask listening port

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ app = create_app()
 
 if __name__ == '__main__':
     # app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
+    #app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
